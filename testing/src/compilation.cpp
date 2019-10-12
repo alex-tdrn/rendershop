e785b7d2f6dd63a64d7077f8c13a1a73f1235903
Alex Todoran <alex-tdrn@protonmail.com>
2019-10-12 18:33:34 +0200

    added test project using catch 2

---
+++ testing/src/compilation.cpp
@@ -0,0 +1,13 @@
+#include "catch.hpp"
+#include "renderdeck/Timer.hpp"
+
+TEST_CASE("compilation.timer", "[factorial]")
+{
+	
+	REQUIRE(1 * 2 == 3);
+}
+
+TEST_CASE("compilation.idontknowhowtostructuretests", "[factorial]")
+{
+	REQUIRE(1 * 2 == 2);
+}
